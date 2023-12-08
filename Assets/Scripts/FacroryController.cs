6e718c81969164eeaf432f798d78802d3e388b8c
r3laty <lolkers.ilya@gmail.com>
2023-12-08 21:58:43 +0300

    Made controller more nicer

--- Assets/Scripts/FacroryController.cs
+++ Assets/Scripts/FacroryController.cs
@@ -1,14 +1,32 @@
+using System.Drawing;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FacroryController : MonoBehaviour
 {
     [SerializeField] private GameObject amogusPrefab;
+
     [SerializeField] private Transform spawnArea;
+    [SerializeField] private Button spawnButton;
+
     [SerializeField] private int[] prices = new int[4];
 
     private Vector2 _needPosition;
     private Vector2 _currentPosition;
-    
+
+    private RectTransform _buttonRect;
+    private TextMeshProUGUI _buttonText;
+    private int _price;
+    private int _priceIndex = 1;
+
+    private string _dollar = "<color=#02c405>$</color>";
+    private void Awake()
+    {
+        _buttonRect = spawnButton.GetComponent<RectTransform>();
+        _buttonText = spawnButton.GetComponentInChildren<TextMeshProUGUI>();
+
+    }
     private void Start()
     {
         _currentPosition = transform.position;
@@ -19,11 +37,31 @@ public class FacroryController : MonoBehaviour
     /// </summary>
     public void SpawnNewAmogus()
     {
-        float checkDistance = Vector3.Distance(_needPosition, _currentPosition);
+        if (_priceIndex < prices.Length)
+        {
+            _price = prices[_priceIndex];
+            _buttonText.text = _price.ToString() + _dollar;
 
-        Vector3 newAmogusSpawnArea = new Vector3(-checkDistance, 0, 0);
+            float checkDistance = Vector3.Distance(_needPosition, _currentPosition);
 
-        Instantiate(amogusPrefab, spawnArea.position, Quaternion.identity);
-        spawnArea.position -= new Vector3(-newAmogusSpawnArea.x, 0, 0);
+            Vector3 newAmogusSpawnArea = new Vector3(-checkDistance, 0, 0);
+
+            Instantiate(amogusPrefab, spawnArea.position, Quaternion.identity);
+            spawnArea.position -= new Vector3(-newAmogusSpawnArea.x, 0, 0);
+            _buttonRect.anchoredPosition -= new Vector2(_buttonRect.sizeDelta.x, 0f);
+
+            _priceIndex++;
+        }
+        else
+        {
+            float checkDistance = Vector3.Distance(_needPosition, _currentPosition);
+
+            Vector3 newAmogusSpawnArea = new Vector3(-checkDistance, 0, 0);
+
+            Instantiate(amogusPrefab, spawnArea.position, Quaternion.identity);
+
+            Destroy(spawnButton.gameObject);
+        }
+
     }
 }
